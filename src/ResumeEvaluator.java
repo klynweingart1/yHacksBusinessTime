07a8bcf56baf3cd162f3937dba623ff8a565b6cd
Elijah Carrel <elijahcarrel@gmail.com>
2015-11-07 15:17:30 -0500

    Updating ResumeEvaluator
    Added findClosestNumber method (not working yet)

--- src/ResumeEvaluator.java
+++ src/ResumeEvaluator.java
@@ -3,15 +3,61 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.regex.*;
+
+import com.sun.jna.platform.win32.WinDef.CHAR;
+
 import java.io.*;
 
 public class ResumeEvaluator {
+	
+	/**
+	 * @param content: text we're passing in
+	 * @param index: index of the word "GPA"
+	 * @param direction: direction to search in (-1 or 1)
+	 * @return start and end indices
+	 *
+	public int[] findClosestNumber(String content, int index, int direction) {
+		while (!Character.isDigit(content.charAt(index))) {
+			index += direction;
+		}
+		int start = index;
+		while ((Character.isDigit(content.charAt(index)) || content.charAt(index) == '.')) {
+			index += direction;
+		}
+		int end = index;
+		int[] pair = {start, end}; 
+		return pair;
+	}
+	*/
+	
+	public String findClosestNumber(String content, int index, int direction) {
+		while (!Character.isDigit(content.charAt(index))) {
+			index += direction;
+		}
+		int start = index;
+		while ((Character.isDigit(content.charAt(index)) || content.charAt(index) == '.')) {
+			index += direction;
+		}
+		int end = index;
+		int[] pair = {start, end}; 
+		return pair;
+	}
 	
 	public String findGPA(String resumeContent) {
 		Pattern gpaPattern = Pattern.compile("(GPA|grade point average)");
 		Matcher gpaMatcher = gpaPattern.matcher(resumeContent);
 		ArrayList<String> gpas = new ArrayList<String>();
 		while (gpaMatcher.find()) {
+	        System.out.print("Start index: " + gpaMatcher.start());
+	        System.out.print(" End index: " + gpaMatcher.end());
+	        System.out.println(" Found: " + gpaMatcher.group());
+	        int[2] prev = findClosestNumber(resumeContent, gpaMatcher.start(), -1);
+	        int[2] next = findClosestNumber(resumeContent, gpaMatcher.end(), 1);
+	        int startIndexPrevNumber = prev[0];
+	        int endIndexPrevNumber = prev[1];
+	        int startIndexNextNumber = next[0];
+	        int endIndexNextNumber = next[1];
+	        String prevString = 
 			gpas.add(gpaMatcher.group(0));
 		}
 		if (gpas.isEmpty())
